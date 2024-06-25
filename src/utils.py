2e9ddd4fea51e9612d7d412dc509141737f3a5ab
durk0123 <153577896+durk0123@users.noreply.github.com>
2024-06-25 13:41:33 -0700

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -9,14 +9,14 @@ def validate_username(username):
 def validate_phone(phone):
     return len(phone) >= 5 and phone.isdigit()
 
-def validate_minecraft_username(mc_username):
-    return 3 <= len(mc_username) <= 16
-
 def validate_domain(domain):
     return re.match(r"^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$", domain)
 
+def validate_auto(auto):
+    return len(auto) >= 1
+
 def is_valid_data_type(data_type):
-    valid_data_types = ['email', 'login', 'phone', 'mc', 'domain']
+    valid_data_types = ['email', 'login', 'phone', 'domain', 'keyword', 'hash', 'auto'] # 'mc' removed due to being removed in API
     return data_type in valid_data_types
 
 def validate_input(data_type, data_value):
@@ -24,9 +24,16 @@ def validate_input(data_type, data_value):
         'email': validate_email,
         'login': validate_username,
         'phone': validate_phone,
-        'mc': validate_minecraft_username,
-        'domain': validate_domain
+        'domain': validate_domain,
+        'auto': validate_auto
     }
 
     validator = validators.get(data_type)
     return validator(data_value) if validator else False
+
+class CustomColors:
+    PURPLE = '\033[38;2;84;71;247m'
+    BLUE = '\033[38;2;61;186;240m'
+    GREEN = '\033[38;2;144;238;144m'
+    RED = '\033[38;2;255;99;71m'
+    RESET = '\033[0m'
